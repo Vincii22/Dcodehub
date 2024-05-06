b784ae5770082419d97151d82b8a9451f5f3006d
vincii <arvingm1522@gmail.com>
2024-05-07 01:45:58 +0800

    Generated Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostCreateComponent } from './post/post-create/post-create.component';
+import { PostEditComponent } from './post/post-edit/post-edit.component';
+import { PostListComponent } from './post/post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostCreateComponent,
+    PostEditComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule
